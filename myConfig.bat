dd1a7c91a11b2f37c10dc2483165d4dcb23dfe92
softwarejedimaster <softwarejedimaster@gmail.com>
2021-02-20 20:21:24 -0500

    initial commit from my personal repository

---
+++ myConfig.bat
@@ -0,0 +1,10 @@
+rem Configuration file to insert the Cobalt Blue color scheme and default configuration files
+
+copy "C:\Users\%username%\AppData\Roaming\Notepad++\themes\Cobalt Blue.xml" "C:\Users\%username%\AppData\Roaming\Notepad++\themes\Cobalt Blue_old.xml" 
+copy active_work-in-progress_Cobalt.xml "C:\Users\%username%\AppData\Roaming\Notepad++\themes\Cobalt Blue.xml"
+rem config_files\config.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
+rem config_files\contextMenu.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
+rem config_files\langs.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
+rem config_files\session.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
+rem config_files\shortcuts.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
+rem config_files\stylers.xml "C:\Users\%username%\AppData\Roaming\Notepad++\
